09439799c463bc723f423e2406a953d911757c75
Eric Silva Rodrigues <95319493+ericsilvarodrigues@users.noreply.github.com>
2022-12-02 13:28:23 -0300

    Update CriancaEsperança.js

--- Exercícios/CriancaEsperança.js
+++ Exercícios/CriancaEsperança.js
@@ -3,8 +3,6 @@
 const read = require('readline-sync')
 const colors = require('colors')
 
-//Variáveis
-
 console.log("===== Criança Esperança =====")
 console.log("[1] para doar R$ 10,00")
 console.log("[2] para doar R$ 25,00")
@@ -12,5 +10,26 @@ console.log("[3] para doar R$ 50,00")
 console.log("[4] para doar outros valores")
 console.log("[5] para cancelar")
 
-let doar = Number(read.question("Selecione o número para doar o valor desejado").replace(",","."))
+//Variáveis
+
+let opcao = Number(read.question("Selecione uma opção: ").replace(",","."))
+let um = 10
+let dois = 25
+let tres = 50 
+let quatro = Number(read.question("Qual seria o outro valor a doar ?").replace(",","."))
+let cinco = 0
+
+//Processamentos
+if(1){
+  console.log( 10,00)
+} else if (2){  
+  console.log( 25.00)
+} else if (3){
+  console.log( 50.00)
+} else if(4){
+  console.log(`${quatro}`)
+} else if(5){
+  console.log(0)
+}
+
 
